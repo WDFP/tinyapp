6d817aaab101b9cb3b0b3bea355050b3abfea0ac
Victor Ha <victorha88@gmail.com>
2022-12-10 14:33:24 -0500

    Store short_url and long_url & redirect /urls to /urls/:id

--- express_server.js
+++ express_server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const generateRandomString = function () {
+  return Math.random().toString(36).substring(2, 8);
+};
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -13,7 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL;
+  if (!longURL) {
+    return res.send("Please provide longURL");
+  }
+
+  const validURL = longURL.startsWith("http")
+  if (!validURL) {
+    return res.send("Please provide valid URL that starts with http");
+  }
+
+  const newId = generateRandomString();
+  urlDatabase[newId] = longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${newId}`); // Respond with redirect to newID
 });
 
 app.get("/urls", (req, res) => {
@@ -25,12 +42,17 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:test", (req, res) => {
-  const shortUrl = req.params.test
+app.get("/urls/:id", (req, res) => {
+  const shortUrl = req.params.id;
   //Use the id from the route parameter to lookup it's associated longURL from the urlDatabase
   // Original Template in M3W6
-  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl]};
+  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.get("/", (req, res) => {
@@ -48,7 +70,3 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-function generateRandomString() {
-  Math.random().toString(6)
-};
